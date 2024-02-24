87c7d990e4fed302ff87472549fe4ff7ea239148
mayur sharma <146337079+mayur976-oss@users.noreply.github.com>
2024-02-24 14:18:46 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,25 @@
+// const passwordbox = document.getElementById('password');
+// const lenght = 12;
+
+// const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+// const number = "0123456789";
+// const symbol = "@#$%^&*()!_+(*^|:)";
+
+// const allchars = upperCase + lowerCase +  number + symbol;
+
+// function createpassword(){
+//     let password = "";
+//     password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//     password += number[Math.floor(Math.random() * number.length)];
+//     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+//     while (lenght >password.length) {
+//         password += allchars[Math.floor(Math.random() * allchars.length)];
+
+//     }
+//     passwordbox.value = password;
+
+// }
+
